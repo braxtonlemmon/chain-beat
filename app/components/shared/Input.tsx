b12ea55b42bb922828c2289d9786bd11eb394df1
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-27 09:00:15 -0600

    Add onChange to Input. Make Button client comp.

--- app/components/shared/Input.tsx
+++ app/components/shared/Input.tsx
@@ -1,14 +1,20 @@
-import {InputHTMLAttributes} from 'react'
+import {ChangeEvent, InputHTMLAttributes} from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string
+  setInputValue: (val: string) => void
 }
 
-function Input({name, ...rest}: InputProps) {
+function Input({name, setInputValue, ...rest}: InputProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {value} = e.target
+    setInputValue(value)
+  }
+
   return (
     <div>
       <label htmlFor={name} />
-      <input id={name} {...rest}></input>
+      <input id={name} onChange={handleChange} {...rest}></input>
     </div>
   )
 }
