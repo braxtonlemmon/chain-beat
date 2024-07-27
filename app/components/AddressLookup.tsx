db1b66c53f006c48cd3ee2cd23b509eee8d760e2
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-27 09:01:14 -0600

    Create AddressLookup component, unstyled.

---
+++ app/components/AddressLookup.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import {ethers, isAddress} from 'ethers'
+
+import Button from './shared/Button'
+import Input from './shared/Input'
+
+type TAddressLookup = {
+  userAddress: string
+  setUserAddress: (userAddress: string) => void
+  setHasAuth: (hasAuth: boolean) => void
+}
+
+function AddressLookup({
+  userAddress,
+  setUserAddress,
+  setHasAuth,
+}: TAddressLookup) {
+  const [isInvalid, setIsInvalid] = useState(false)
+
+  // Checks if entered address is valid Ethereum userAddress
+  const handleSubmit = async () => {
+    const isValid = isAddress(userAddress)
+    setIsInvalid(!isValid)
+    if (isValid) {
+      setHasAuth(true)
+    }
+  }
+
+  return (
+    <div className="container flex flex-col">
+      <p>enter userAddress:</p>
+      <Input
+        name="userAddress"
+        value={userAddress}
+        setInputValue={setUserAddress}
+      />
+      {userAddress.length > 0 && isInvalid && <p>ERROR</p>}
+      <Button onClick={() => handleSubmit()}>submit</Button>
+    </div>
+  )
+}
+
+export default AddressLookup
