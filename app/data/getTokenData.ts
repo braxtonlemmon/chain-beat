b4c3f6d0e2437e3504229f4135cc5e162e482f87
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-27 18:30:57 -0600

    Create getTokenData server action, add coinmarketcap API key

---
+++ app/data/getTokenData.ts
@@ -0,0 +1,51 @@
+'use server'
+
+import {config} from '../../config'
+
+type TCoinMarketCapResponse = {
+  data: {
+    [id: string]: {
+      name: string
+      symbol: string
+      quote: {
+        USD: {
+          price: number
+          percent_change_24h: number
+        }
+      }
+    }
+  }
+}
+
+type TTokenData = {
+  tokenName: string
+  tokenSymbol: string
+  tokenPrice: number
+  tokenChange24h: number
+}
+
+export async function getTokenData(tokenId: string): TTokenData {
+  const apiKey = config.coinmarketcapApiKey
+  const url = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=${tokenId}`
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-CMC_PRO_API_KEY': apiKey,
+    },
+  })
+  const data: TCoinMarketCapResponse = await res.json()
+  const {
+    name,
+    symbol,
+    quote: {
+      USD: {price, percent_change_24h},
+    },
+  } = data.data[tokenId]
+
+  return {
+    tokenName: name,
+    tokenSymbol: symbol,
+    tokenPrice: price,
+    tokenChange24h: percent_change_24h,
+  }
+}
--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const config = {
   etherscanApiKey: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY || '',
+  coinmarketcapApiKey: process.env.NEXT_PUBLIC_COINMARKETCAP_API_KEY || '',
 }
