f5c6bfe9930ca20fd689e1c0de2dc723088fd82f
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-29 14:51:38 -0600

    Create Header component and add to top-level layout

---
+++ app/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto flex items-center justify-between p-6">
+        <div>
+          <h1 className="text-2xl font-bold">Chain Beat</h1>
+        </div>
+        <div className="flex gap-4">
+          <Link href="/">Address Lookup</Link>
+          <Link href="/account">Account</Link>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
