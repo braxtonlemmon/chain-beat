0e30febc82662f677fa08e8e5af116a9a5bbf58c
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-30 15:21:21 -0600

    Header styling and refactor

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,35 @@
 import Link from 'next/link'
 
 function Header() {
+  const links = [
+    {
+      href: '/address-lookup',
+      label: 'ADDRESS LOOKUP',
+    },
+    {
+      href: '/account',
+      label: 'ACCOUNT',
+    },
+  ]
+
   return (
     <header className="bg-header font-notoSans">
-      <nav className="mx-auto flex items-center justify-between p-6">
+      <nav className="mx-auto flex items-center justify-between px-6 py-4">
         <div>
-          <h1 className="text-2xl font-fjalla">CHAIN BEAT</h1>
+          <h1 className="text-[36px] font-fjalla">CHAIN BEAT</h1>
         </div>
         <div className="flex gap-4">
-          <Link href="/address-lookup">Address Lookup</Link>
-          <Link href="/account">Account</Link>
+          {links.map((link) => {
+            return (
+              <Link
+                key={link.href}
+                href={link.href}
+                className="border-double border-4 border-primaryText rounded-lg p-[6px] hover:scale-[1.05]"
+              >
+                {link.label}
+              </Link>
+            )
+          })}
         </div>
       </nav>
     </header>
