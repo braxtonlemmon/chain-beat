178305cb9063529c6f9a8266b094b4ec9c88f402
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-30 14:29:45 -0600

    Create CopyItem component for tx history table

---
+++ app/components/CopyItem.tsx
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react'
+import {CheckCircle, Copy} from 'react-feather'
+
+type TCopyItem = {
+  textToCopy: string
+}
+
+function CopyItem({textToCopy}: TCopyItem) {
+  const [hasCopied, setHasCopied] = useState(false)
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(textToCopy)
+    setHasCopied(true)
+  }
+
+  useEffect(() => {
+    if (hasCopied) {
+      const timeout = setTimeout(() => {
+        setHasCopied(false)
+      }, 1500)
+      return () => clearTimeout(timeout)
+    }
+  }, [hasCopied])
+
+  return (
+    <>
+      {hasCopied ? (
+        <CheckCircle size={16} />
+      ) : (
+        <Copy size={16} cursor="pointer" onClick={() => copyToClipboard()} />
+      )}
+    </>
+  )
+}
+
+export default CopyItem
