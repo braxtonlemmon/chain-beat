2afb6e31c6a6752be22f4f56a59b0f84de8803da
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-30 14:35:07 -0600

    add more styling to Input, Card, Button

--- app/components/shared/Button.tsx
+++ app/components/shared/Button.tsx
@@ -4,7 +4,14 @@ import {ButtonHTMLAttributes} from 'react'
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 function Button({children, ...rest}: ButtonProps) {
-  return <button {...rest}>{children}</button>
+  return (
+    <button
+      className="bg-accent rounded-md p-2 px-4 hover:bg-lightAccent disabled:bg-background disabled:text-cardBackground uppercase"
+      {...rest}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- app/components/shared/Input.tsx
+++ app/components/shared/Input.tsx
@@ -3,18 +3,24 @@ import {ChangeEvent, InputHTMLAttributes} from 'react'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string
   setInputValue: (val: string) => void
+  label?: string
 }
 
-function Input({name, setInputValue, ...rest}: InputProps) {
+function Input({name, setInputValue, label, ...rest}: InputProps) {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const {value} = e.target
     setInputValue(value)
   }
 
   return (
-    <div>
-      <label htmlFor={name} />
-      <input id={name} onChange={handleChange} {...rest}></input>
+    <div className="w-full">
+      <label htmlFor={name}>{label}</label>
+      <input
+        className="p-2 rounded text-background w-full"
+        id={name}
+        onChange={handleChange}
+        {...rest}
+      ></input>
     </div>
   )
 }
