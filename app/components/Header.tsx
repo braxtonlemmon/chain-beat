1e8b3842eb006addf0183bef8f892682130eb74a
braxtonlemmon <braxtonlemmon@gmail.com>
2024-07-31 10:05:11 -0600

    Header style tweaks

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import logo from '../../public/icon.svg'
+import Image from 'next/image'
 
 function Header() {
   const links = [
     {
       href: '/address-lookup',
-      label: 'ADDRESS LOOKUP',
+      label: 'TRANSACTIONS',
     },
     {
       href: '/account',
@@ -15,8 +17,16 @@ function Header() {
   return (
     <header className="bg-header font-notoSans">
       <nav className="mx-auto flex items-center justify-between px-6 py-4">
-        <div>
-          <h1 className="text-[36px] font-fjalla">CHAIN BEAT</h1>
+        <div className="flex gap-3 items-center select-none">
+          <div className="block md:hidden">
+            <Image priority src={logo} alt="Chain Beat logo" height={42} />
+          </div>
+          <div className="hidden md:block">
+            <Image priority src={logo} alt="Chain Beat logo" height={36} />
+          </div>
+          <h1 className="text-2xl md:text-[36px] font-fjalla text-center">
+            CHAIN BEAT
+          </h1>
         </div>
         <div className="flex gap-4">
           {links.map((link) => {
@@ -24,7 +34,7 @@ function Header() {
               <Link
                 key={link.href}
                 href={link.href}
-                className="border-double border-4 border-primaryText rounded-lg p-[6px] hover:scale-[1.05]"
+                className="text-sm md:text-md border-solid border-2 border-primaryText rounded-lg p-2 m:p-[6px] hover:scale-[1.03] transition-all text-center flex items-center justify-center bg-[white] text-cardBackground m:px-4"
               >
                 {link.label}
               </Link>
